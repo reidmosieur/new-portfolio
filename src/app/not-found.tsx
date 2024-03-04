47442ca01bd6ca398bb4e85337330a7cc1df194d
reid <rmosieur@gmail.com>
2024-03-04 08:18:32 -0500

    meta fixes

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -11,12 +11,25 @@ const Page = () => {
                 href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap"
                 rel="stylesheet"
               />
+              <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+              <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+              <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+              <link rel="manifest" href="/site.webmanifest"></link>
+              <title>Reid Mosieur | Full Stack Dev</title>
+              <meta name="description" content="Hi, I'm Reid, a full stack developer ready to do what it takes to increase conversions, user retention, and reduce the cost of development.Hi, I'm a full stack developer ready to increase conversions, retention, and reduce the cost of development without losing usability, security, or performance." />
+              <meta property="og:locale" content="en_US" />
+              <meta property="og:title" content="Reid Mosieur | Full Stack Dev" />
+              <meta property="og:url" content={"https://www.reidmosieur.dev"} />
+              <meta property="og:site_name" content="Reid Mosieur" />
+              <meta name="viewport" content="width=device-width, initial-scale=1" />
+              <meta property="og:image" content={'https://www.reidmosieur.dev/reid-mosieur-full-stack-developer.png'} />
+              <meta property="og:image:secure_url" content={'https://www.reidmosieur.dev/reid-mosieur-full-stack-developer.png'} />
           </head>
           <body>
             <main>
                 <div className="min-h-screen px-1 flex flex-col flex-wrap content-center justify-center gap-4 lg:gap-8 text-white" >
                     <h1 className="text-xl sm:text-3xl lg:text-4xl font-extrabold" >Let&apos;s get you back on track shall we?</h1>
-                    <div className="w-fit mx-auto flex flex-col md:flex-row flex-wrap divide-y-2 md:divide-x-2 divide-stone-700" >
+                    <div className="w-fit mx-auto flex flex-col md:flex-row flex-wrap divide-y-2 md:divide-y-0 md:divide-x-2 divide-stone-700" >
                         {links.internals.map(link => (
                             <Link
                                 key={link.href}
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,32 @@
 import { AppProps } from 'next/app';
 import 'tailwindcss/tailwind.css';
 import "../app/globals.css";
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (    
-    <main>
-      <div className='w-full min-h-screen' >
-          <Component {...pageProps} />
-      </div>
-    </main>
+    <>
+      <Head>
+          <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+          <link rel="manifest" href="/site.webmanifest"></link>
+          <title>Reid Mosieur | Full Stack Dev</title>
+          <meta name="description" content="Hi, I'm Reid, a full stack developer ready to do what it takes to increase conversions, user retention, and reduce the cost of development.Hi, I'm a full stack developer ready to increase conversions, retention, and reduce the cost of development without losing usability, security, or performance." />
+          <meta property="og:locale" content="en_US" />
+          <meta property="og:title" content="Reid Mosieur | Full Stack Dev" />
+          <meta property="og:url" content={"https://www.reidmosieur.dev"} />
+          <meta property="og:site_name" content="Reid Mosieur" />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <meta property="og:image" content={'https://www.reidmosieur.dev/reid-mosieur-full-stack-developer.png'} />
+          <meta property="og:image:secure_url" content={'https://www.reidmosieur.dev/reid-mosieur-full-stack-developer.png'} />
+        </Head>
+        <main>
+          <div className='w-full min-h-screen' >
+              <Component {...pageProps} />
+          </div>
+        </main>
+    </>
   );
 }
 
